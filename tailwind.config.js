94a11e0ecd4bd25fcc7dda29204c018a0d7a564e
anecha <anecha@anechas-MacBook-Pro.local>
2026-01-05 01:18:08 +0700

    css

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,17 @@ export default {
       fontFamily: {
         outfit: ['Outfit', 'system-ui', 'sans-serif'],
       },
+      colors: {
+        'main-bg': 'rgb(var(--color-bg-main) / <alpha-value>)',
+        'card-bg': 'rgb(var(--color-bg-card) / <alpha-value>)',
+        'title-text': 'rgb(var(--color-text-title) / <alpha-value>)',
+        'body-text': 'rgb(var(--color-text-body) / <alpha-value>)',
+        'border-color': 'rgb(var(--color-border) / <alpha-value>)',
+        'nav-bg': 'rgb(var(--color-bg-nav) / <alpha-value>)',
+      },
     },
   },
   plugins: [],
+  darkMode: "class",
 }
 
